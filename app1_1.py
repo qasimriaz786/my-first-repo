73c28ba1f30481ad3e92da17ad984bd66990fe4c
qasimriaz786 <s2023065016@umt.edu.pk>
2026-04-26 23:10:39 +0500

    Refactored: extracted amount_for function

--- app1_1.py
+++ app1_1.py
@@ -13,8 +13,7 @@ def statement(invoice, plays):
     result = f"Statement for {invoice['customer']}\n"
 
     for perf in invoice['performances']:
-        play = plays[perf['playID']]
-        this_amount = 0
+        
 
         # Calculate amount
         if play['type'] == "tragedy":
